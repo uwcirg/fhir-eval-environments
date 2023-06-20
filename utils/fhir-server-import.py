e9b6a07d304a6ddf9dd8b510c218dbed4130e3a4
Ivan Cvitkovic <ivanc@uw.edu>
2023-06-20 06:53:44 -0700

    code style changes

--- utils/fhir-server-import.py
+++ utils/fhir-server-import.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Util to upload all given, or available FHIR resources to the given FHIR server"""
 import requests
+
 base_url = "https://hapi.128.208.230.197.nip.io/fhir"
 url = f"{base_url}/$import"
 # {fileNumber}.{ResourceType}.ndjson
 payload = {
     "inputFormat": "application/fhir+ndjson",
-    # URI for tracking this set of imported data throughout its lifecycle. 
+    # URI for tracking this set of imported data throughout its lifecycle.
     # MAY be used to specify a FHIR endpoint that can by the importing system when matching references to previously imported data.
     "inputSource": base_url,
     "storageDetail": { "type": "https" },
@@ -75,10 +76,9 @@ payload = {
 }
 
 
-
 headers = {
-    'Accept': 'application/fhir+json',
-    'Prefer': 'respond-async',
+    "Accept": "application/fhir+json",
+    "Prefer": "respond-async",
     #'Content-Type': 'application/json',
     #'Authorization': 'Bearer <Auth Token>'
 }
