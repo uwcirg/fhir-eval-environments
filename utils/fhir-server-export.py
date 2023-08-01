4ac2a8767a88eee8f4c31dd968171334d313b259
Ivan Cvitkovic <ivanc@uw.edu>
2023-08-01 11:45:38 -0700

    cleanup

--- utils/fhir-server-export.py
+++ utils/fhir-server-export.py
@@ -1,119 +1,101 @@
 #!/usr/bin/env python3
 """Util to upload all given, or available FHIR resources to the given FHIR server"""
-import requests, sys
+import requests, sys, time
 
-base_url= sys.argv.pop()
+def poll_status(status_poll_url):
+    # in seconds
+    max_rety_time = 10*60
+    rety_time = 0
 
-# base_url = "https://hapi.128.208.230.197.nip.io/fhir"
-import_url = f"{base_url}/$import"
+    while rety_time < max_rety_time:
+        status_poll_response = requests.get(status_poll_url)
+        status_poll_response.raise_for_status()
 
-# https://smilecdr.com/docs/bulk/fhir_bulk_import.html#triggering-a-bulk-import
-payload = {
-    "resourceType": "Parameters",
-    "parameter": [
-        {
-            "name": "inputFormat",
-            "valueCode": "application/fhir+ndjson"
-        }, 
-        {
-            "name": "inputSource",
-            "valueUri": base_url,
-        }, 
-        {
-            "name": "storageDetail",
-            "part": [
-                {
-                    "name": "type",
-                    "valueCode": "https"
-                }, 
-                {
-                    "name": "maxBatchResourceCount",
-                    "valueString": "1000"
-                } 
-            ]
-        }, 
-        # {
-            # "name": "input",
-            # "part": [
-                # {
-                    # "name": "type",
-                    # "valueCode": "Measure"
-                # }, 
-                # {
-                    # "name": "url",
-                    # "valueUri": "https://raw.githubusercontent.com/uwcirg/fhir-eval-environments/bfa6570/tests/data/21340.Measure.ndjson"
-                # } 
-            # ]
-        # } 
-    ]
-}
+        retry_after = int(status_poll_response.headers.get("Retry-After", 0))
 
+        if not retry_after:
+            return status_poll_response.json()
 
-input_base_url = "https://raw.githubusercontent.com/uwcirg/fhir-eval-environments/22d319d/"
+        progress = status_poll_response.headers.get("X-Progress")
+        if progress:
+            print("progress: ", progress)
 
+        print(f"waiting {retry_after} seconds")
+        rety_time += retry_after
+        time.sleep(retry_after)
 
-export_resources = (
-    "tests/data/21340.Measure.ndjson",
-    "tests/data/21364.Organization.ndjson",
-    "tests/data/21365.Location.ndjson",
+def download_file(url, filename=None):
+    if not filename:
+        filename = url.split('/')[-1]
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                #if chunk: 
+                f.write(chunk)
+    return filename
 
-    "tests/data/21318.MeasureReport.ndjson",
-    "tests/data/21319.MeasureReport.ndjson",
-    "tests/data/21320.MeasureReport.ndjson",
-    "tests/data/21321.MeasureReport.ndjson",
-    "tests/data/21322.MeasureReport.ndjson",
-    "tests/data/21323.MeasureReport.ndjson",
-    "tests/data/21324.MeasureReport.ndjson",
-    "tests/data/21325.MeasureReport.ndjson",
-    "tests/data/21326.MeasureReport.ndjson",
-    "tests/data/21327.MeasureReport.ndjson",
-    "tests/data/21328.MeasureReport.ndjson",
-    "tests/data/21329.MeasureReport.ndjson",
-    "tests/data/21330.MeasureReport.ndjson",
-    "tests/data/21331.MeasureReport.ndjson",
-    "tests/data/21332.MeasureReport.ndjson",
-    "tests/data/21333.MeasureReport.ndjson",
-    "tests/data/21334.MeasureReport.ndjson",
-    "tests/data/21335.MeasureReport.ndjson",
-    "tests/data/21336.MeasureReport.ndjson",
-    "tests/data/21337.MeasureReport.ndjson",
-    "tests/data/21338.MeasureReport.ndjson",
-)
+def main():
+    base_url= sys.argv.pop()
 
-input_file_parameters = []
-for resource in export_resources:
-    resource_type = resource.split(".")[-2]
-    full_url = f"{input_base_url}{resource}"
-
-    param = {
-        "name": "input",
-        "part": [
-            {
-                "name": "type",
-                "valueCode": resource_type
-            },
-            {
-                "name": "url",
-                "valueUri": full_url
-            }
-        ]
+    headers = {
+        "Accept": "ndjson",
+        "Prefer": "respond-async",
+        #'Authorization': 'Bearer <Auth Token>'
     }
 
-    input_file_parameters.append(param)
+    kickoff_response = requests.post(
+        url=f"{base_url}/$export",
+        headers=headers,
+    )
 
-payload["parameter"].extend(input_file_parameters)
+    # raise exceptions when response status is not 2XX
+    kickoff_response.raise_for_status()
 
-headers = {
-    "Accept": "application/fhir+json",
-    "Prefer": "respond-async",
-    #'Authorization': 'Bearer <Auth Token>'
-}
+    status_poll_url = kickoff_response.headers["Content-Location"]
+    # Hapi ignores server_name configuration
+    if base_url.startswith("https://"):
+        status_poll_url = status_poll_url.replace("http://", "https://")
+    print(status_poll_url)
 
-export_status_response = requests.request(
-    method="POST",
-    url=import_url,
-    headers=headers,
-    json=payload,
-)
+    complete_json = poll_status(status_poll_url)
 
-print(export_status_response.text)
+    for file_item in complete_json.get("output"):
+        local_filename = ".".join((
+            file_item["url"].split("/")[-1],
+            file_item["type"],
+            "ndjson",
+        ))
+        print("downloading: ", file_item["url"])
+        download_file(url=file_item["url"], filename=local_filename)
+
+    errors = complete_json.get("errors")
+    if errors:
+        print(errors)
+
+if __name__ == "__main__":
+    main()
+
+# FHIR_BASE_URL='https://hapi.128.208.230.197.nip.io/fhir'
+
+# export_poll_url=$(curl ${FHIR_BASE_URL}'/$export?_type=Measure,MeasureReport,QuestionnaireResponse' \
+    # -H 'Prefer: respond-async' \
+    # -H 'Accept: ndjson' -v \
+    # -o /dev/null 2>&1 | grep content-location | cut -d ' ' -f3)
+
+# export_poll_url=$(echo $export_poll_url | sed 's|http:|https:|g')
+# export_poll_url=$(echo $export_poll_url | sed 's|\r||g')
+
+# export_poll_url=$(echo $export_poll_url | sed 's|\$|%24|g')
+# export_poll_url=$(echo -n $export_poll_url | tr -d '\r')
+
+# echo $export_poll_url > out.bar
+
+# echo $export_poll_url
+# sleep 1m
+
+#temp_dir="$(mktemp --directory --suffix="$cmdname")"
+# curl "$export_poll_url" | grep url | cut -d'"' -f4 | xargs wget
