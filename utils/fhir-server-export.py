588b77d45285dd8290a21f879d34a4ff7878778c
Ivan Cvitkovic <ivanc@uw.edu>
2023-08-01 11:46:47 -0700

    cleanup

--- utils/fhir-server-export.py
+++ utils/fhir-server-export.py
@@ -79,26 +79,6 @@ def main():
     if errors:
         print(errors)
 
+
 if __name__ == "__main__":
     main()
-
-# FHIR_BASE_URL='https://hapi.128.208.230.197.nip.io/fhir'
-
-# export_poll_url=$(curl ${FHIR_BASE_URL}'/$export?_type=Measure,MeasureReport,QuestionnaireResponse' \
-    # -H 'Prefer: respond-async' \
-    # -H 'Accept: ndjson' -v \
-    # -o /dev/null 2>&1 | grep content-location | cut -d ' ' -f3)
-
-# export_poll_url=$(echo $export_poll_url | sed 's|http:|https:|g')
-# export_poll_url=$(echo $export_poll_url | sed 's|\r||g')
-
-# export_poll_url=$(echo $export_poll_url | sed 's|\$|%24|g')
-# export_poll_url=$(echo -n $export_poll_url | tr -d '\r')
-
-# echo $export_poll_url > out.bar
-
-# echo $export_poll_url
-# sleep 1m
-
-#temp_dir="$(mktemp --directory --suffix="$cmdname")"
-# curl "$export_poll_url" | grep url | cut -d'"' -f4 | xargs wget
