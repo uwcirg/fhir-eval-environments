8b1d462f55fc96f262a4fd38f2c6c45b6e1cd837
Ivan Cvitkovic <ivanc@uw.edu>
2023-08-04 19:37:30 -0700

    Add workaround when FHIR server_name doesn't match public URL

--- utils/fhir-server-export.py
+++ utils/fhir-server-export.py
@@ -19,6 +19,24 @@ def download_file(url, filename=None):
     return filename
 
 
+def fixup_url(url, base_url):
+    """
+    Replace FHIR base URL in given FHIR API call with different base_url
+    Helpful when a FHIR server is configured with a server_name that does not match its public one
+    """
+    if not url.startswith(base_url):
+        second_last_path, last_path = url.split("/")[-2:]
+        # FHIR operations
+        if last_path.startswith("$"):
+            return f"{base_url}/{last_path}"
+
+        # known resources (Hapi FHIR uses Binary)
+        if second_last_path in ("Binary"):
+            return f"{base_url}/{second_last_path}/{last_path}"
+
+    return url
+
+
 def poll_status(status_poll_url):
     """Poll given status URL until ready (or timeout). Returns response JSON when ready to download"""
     # TODO set via argparse
@@ -70,16 +88,18 @@ def main():
     if base_url.startswith("https://"):
         status_poll_url = status_poll_url.replace("http://", "https://")
 
-    complete_json = poll_status(status_poll_url)
+    complete_json = poll_status(fixup_url(url=status_poll_url, base_url=base_url))
     for file_item in complete_json.get("output"):
+        url = fixup_url(url=file_item["url"], base_url=base_url)
+
         # TODO allow passing directory via argparse
         local_filename = ".".join((
-            file_item["url"].split("/")[-1],
+            url.split("/")[-1],
             file_item["type"],
             "ndjson",
         ))
-        print("downloading: ", file_item["url"])
-        download_file(url=file_item["url"], filename=local_filename)
+        print("downloading: ", url)
+        download_file(url=url, filename=local_filename)
         print("saved to: ", local_filename)
 
     errors = complete_json.get("errors")
